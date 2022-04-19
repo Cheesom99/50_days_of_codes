f605ace0e429a3016165f87e5e8aed3dae29fcc3
Cheesom <obichisom99@gmail.com>
2022-04-19 11:24:19 +0100

    Commented more on the codes

--- Day4.py
+++
@@ -1,8 +0,0 @@
-# Mad Libs game
-
-colour = input("Input a colour: ")
-animal = input("Type an animal: ")
-number = int(input("A digit: "))
-country = input("Type a country: ")
-
-print("I saw a ", colour, animal+". It has", number, "legs but I wish", country, "hosted the Olympics.")
